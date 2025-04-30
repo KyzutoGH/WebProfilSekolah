d1871b917127251f9d15865624a1f79e3bd7f4b8
KyzutoGH <iksancjr@gmail.com>
2025-05-01 06:52:43 +0700

    redeploy fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,7 +11,7 @@ class UserSeeder extends Seeder
     public function run(): void 
     {
         User::firstOrCreate(
-            ['email' => 'smppgribakung@gmail.com'], // Cek dulu berdasarkan email
+            ['email' => 'smppgribakung@gmail.com'],
             [
                 'name' => 'Admin Smp Pgri',
                 'password' => bcrypt('smppgri1984'),
