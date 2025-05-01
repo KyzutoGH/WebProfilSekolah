15353c1f9e705b3e5839758dd2e9fdd3e4aa7d2d
KyzutoGH <iksancjr@gmail.com>
2025-05-01 07:12:51 +0700

    blogseeder offkan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [
                 // UserSeeder::class,
-                BlogSeeder::class,
+                // BlogSeeder::class,
                 ContactSeeder::class,
                 PhotoSeeder::class,
                 PpdbSeeder::class
